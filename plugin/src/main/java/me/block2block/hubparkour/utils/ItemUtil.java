e7a7b22bf41ae941bde8547e2fae960a2ff74ad7
Block2Block <accounts@block2block.me>
2025-02-17 05:53:56 +0800

    v2.8.2: Add support for custom model data and bugfixes  - Added support for custom model data in parkour items (1.14+)  - Internal Spigot API version has been updated to 1.14.  - Fixed an issue where the MySQL tables would generate incorrectly and resultantly cause issues loading parkours.  - Fixed an issue where MySQL would reject a database schema update due to incorrect query.  - Fixed an issue where you couldn't delete parkours that were in a world that didn't exist anymore.  - Fixed an exploit where players could put items in the cursor to prevent them being removed when players inventory is cleared.  - Hopefully resolved issues with holograms not disappearing until a server restart if a parkour is deleted
    Signed-off-by: Block2Block <accounts@block2block.me>
    
    (cherry picked from commit 6a931ff749e2c74623ccdb9e7e36eda3976ee62c)
    Signed-off-by: HappyAreaBean <happyareabean@proton.me>

--- plugin/src/main/java/me/block2block/hubparkour/utils/ItemUtil.java
+++ plugin/src/main/java/me/block2block/hubparkour/utils/ItemUtil.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 @SuppressWarnings("deprecation")
 public class ItemUtil {
 
-    public static ItemStack ci(Material type, String name, int amount, String lore, short data, String skullName, String action) {
+    public static ItemStack ci(Material type, String name, int amount, String lore, short data, int customModelData, String skullName, String action) {
         ItemStack is = new ItemStack(type, amount, data);
         ItemMeta im = is.getItemMeta();
         im.setDisplayName(HubParkour.c(false, name));
@@ -31,6 +31,15 @@ public class ItemUtil {
             im.setUnbreakable(true);
         }
 
+        if (HubParkour.isPost1_14()) {
+            if (customModelData != -1) {
+                im.setCustomModelData(customModelData);
+            } else {
+                im.setCustomModelData(null);
+            }
+        }
+
+
         im.addItemFlags(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ATTRIBUTES);
         is.setItemMeta(im);
 
@@ -39,14 +48,17 @@ public class ItemUtil {
         }
         return is;
     }
-    public static ItemStack ci(Material type, String name, int amount, String lore, short data, String action) {
-        return ci(type, name, amount, lore, data, null, action);
+
+    public static ItemStack ci(Material type, String name, int amount, String lore, short data, int customModelData, String action) {
+        return ci(type, name, amount, lore, data, customModelData, null, action);
     }
+
     public static ItemStack ci(Material type, String name, int amount, String lore, short data) {
-        return ci(type, name, amount, lore, data, null, null);
+        return ci(type, name, amount, lore, data, -1, null, null);
     }
+
     public static ItemStack ci(Material type, String name, int amount, String lore) {
-        return ci(type, name, amount, lore, (short)0, null);
+        return ci(type, name, amount, lore, (short)0);
     }
     public static ItemStack ci(Material type, String name, int amount) {
         return ci(type, name, amount, null);
